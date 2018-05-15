371fda3b1de1d838801aed688c3d2eb0f1a67178
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-05-15 14:08:16 +0200

    Implement classes SingleValue and MultipleValue

---
+++ src/main/java/com/codecool/java/expertsystem/MultipleValue.java
@@ -0,0 +1,28 @@
+package main.java.com.codecool.java.expertsystem;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MultipleValue extends Value {
+
+    private List<String> params;
+    private boolean selectionType;
+
+    public MultipleValue(List<String> params, boolean selectionType) {
+        
+        this.params = params;
+        this.selectionType = selectionType;
+    }
+
+    public List<String> getInputPattern() {
+
+        return params;
+    }
+
+    public boolean getSelectionType() {
+
+        return this.selectionType;
+    }
+}
