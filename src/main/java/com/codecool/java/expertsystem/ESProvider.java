18f32529cb9712698557c360685863f7ddebbd3c
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-05-15 14:15:09 +0200

    Implement framework of class ESProvider

---
+++ src/main/java/com/codecool/java/expertsystem/ESProvider.java
@@ -0,0 +1,31 @@
+package main.java.com.codecool.java.expertsystem;
+
+public class ESProvider {
+
+    RuleRepository ruleRepository;
+    FactRepository factRepository;
+    
+
+    public ESProvider(FactParser factParser, RuleParser ruleParser) {
+
+        ruleRepository = ruleParser.getRuleRepository();
+        factRepository = factParser.getFactRepository();
+    }
+
+    public void collectAnswers() {
+
+        //to implement
+    }
+
+    public boolean getAnswerByQuestion(String questionId) {
+
+        //to implement
+        return false;
+    }
+
+    public String evaluate() {
+
+        //to implement
+        return "";
+    }
+}
