fa1b49e7316bda9e933f92c98f07ba24314dc57e
idzia <ania.idzi@gmail.com>
2018-05-17 00:35:16 +0200

    Add Iterator test

--- src/main/java/com/codecool/java/expertsystem/Main.java
+++ src/main/java/com/codecool/java/expertsystem/Main.java
@@ -1,8 +1,23 @@
 package com.codecool.java.expertsystem;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
-        RuleParser myrules = new RuleParser();       
+        //System.out.println("Test");
+        RuleParser myrules = new RuleParser();   
+        while (myrules.getRuleRepository().getIterator().hasNext()) {
+            
+            String questionToPrint = myrules.getRuleRepository().getIterator().next().getQuestion();
+            getString(questionToPrint);
+            
+        }
+          
     }
+
+    public static String getString(String text) {
+        System.out.println(text);
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    } 
 }
