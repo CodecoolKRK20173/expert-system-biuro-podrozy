c890e93145a1ed91c2baae07f3bf425e00a0f41d
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-05-16 14:44:57 +0200

    Remove commented code & unsued imports

--- src/main/java/com/codecool/java/expertsystem/XMLParser.java
+++ src/main/java/com/codecool/java/expertsystem/XMLParser.java
@@ -1,11 +1,7 @@
 package com.codecool.java.expertsystem;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.File;
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -25,21 +21,4 @@ public abstract class XMLParser {
             e.printStackTrace();
         }
     }
-
-    //protected String xmlString;
-    
-    // public void loadXmlDocument(String xmlPath) {
-        
-    //     StringBuilder sb = new StringBuilder();
-    //     try (BufferedReader br = new BufferedReader(new FileReader(xmlPath))) {
-    //         String sCurrentLine;
-    //         while ((sCurrentLine = br.readLine()) != null) {
-    //             sb.append(sCurrentLine);
-    //         }
-    //         this.xmlString = sb.toString();
-    //     } catch (IOException e) {
-    //         e.printStackTrace();
-    //     }
-    // }
-
 }
