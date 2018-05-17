8199edcdb6a21c0e24b38ecf78ade85989424f21
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-05-17 13:39:00 +0200

    Add & implement user input validation

--- src/main/java/com/codecool/java/expertsystem/ESProvider.java
+++ src/main/java/com/codecool/java/expertsystem/ESProvider.java
@@ -10,7 +10,7 @@ public class ESProvider {
     RuleRepository ruleRepository;
     FactRepository factRepository;
 
-    private Map<String, String> userAnswers;
+    private Map<String, Boolean> userAnswers;
 
     public ESProvider(FactParser factParser, RuleParser ruleParser) {
         ruleRepository = ruleParser.getRuleRepository();
@@ -19,19 +19,32 @@ public class ESProvider {
     }
 
     public void collectAnswers() {
-        Scanner in = new Scanner(System.in);
         while (ruleRepository.getIterator().hasNext()) {
-            Question q = ruleRepository.getIterator().next();
-            System.out.print("\n" + q.getQuestion() + " :");
-            String answer = in.nextLine();
-            userAnswers.put(q.getId(), answer);
+            Question question = ruleRepository.getIterator().next();
+            userAnswers.put(question.getId(), validateUserInput(question));
             System.out.println(userAnswers); // test use only TODO: delete for production
         }
     }
 
+    private Boolean validateUserInput(Question q) {
+        Scanner in = new Scanner(System.in);
+        boolean success = false;
+        Boolean validatedAnswer = null;
+        while (!success) {
+            try {
+                System.out.print("\n" + q.getQuestion());
+                String answer = in.nextLine();
+                validatedAnswer = q.getEvalutedAnswer(answer);
+                success = true;
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return validatedAnswer;
+    }
+
     public boolean getAnswerByQuestion(String questionId) {
-//        return this.userAnswers.get(questionId);
-        return false;
+        return this.userAnswers.get(questionId);
     }
 
     public String evaluate() {
