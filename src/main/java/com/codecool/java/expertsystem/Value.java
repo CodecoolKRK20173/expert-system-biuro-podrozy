a373cb2e5175920e23c391060208c0f71c7888d3
angelikanieduziak <angelika.nieduziak@gmail.com>
2018-05-17 12:17:42 +0200

    Remove comments

--- src/main/java/com/codecool/java/expertsystem/Value.java
+++ src/main/java/com/codecool/java/expertsystem/Value.java
@@ -4,12 +4,10 @@ import java.util.List;
 
 public abstract class Value {
 
-    //private String param;
     protected boolean selectionType;
 
     public abstract List<String> getInputPattern();
   
-    //public abstract boolean getSelectionType();
     public boolean getSelectionType() {
         return this.selectionType;
     }
