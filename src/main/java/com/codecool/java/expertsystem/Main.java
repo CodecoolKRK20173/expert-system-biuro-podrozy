94793423bac9daa0cfdca1ce999dd7bbeca6419b
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-05-17 12:13:04 +0200

    Implement main WIP

--- src/main/java/com/codecool/java/expertsystem/Main.java
+++ src/main/java/com/codecool/java/expertsystem/Main.java
@@ -1,23 +1,13 @@
 package com.codecool.java.expertsystem;
 
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Test");
-        RuleParser myrules = new RuleParser();   
-        while (myrules.getRuleRepository().getIterator().hasNext()) {
-            
-            String questionToPrint = myrules.getRuleRepository().getIterator().next().getQuestion();
-            getString(questionToPrint);
-            
-        }
-          
-    }
 
-    public static String getString(String text) {
-        System.out.println(text);
-        Scanner input = new Scanner(System.in);
-        return input.nextLine();
-    } 
+        FactParser factParser = new FactParser();
+        RuleParser ruleParser = new RuleParser();
+
+        ESProvider esProvider = new ESProvider(factParser, ruleParser);
+        esProvider.collectAnswers();
+    }
 }
